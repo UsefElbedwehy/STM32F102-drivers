3ff0e7e386cb417fc6524f32a471e5a723e8ad9d
UsefElbedwehy <elbedwehyy@gmail.com>
2024-03-07 13:24:40 +0200

    update CLCD driver

--- 03-HAL/03-CLCD/Inc/CLCD_interface.h
+++ 03-HAL/03-CLCD/Inc/CLCD_interface.h
@@ -1,9 +1,80 @@
 #ifndef   CLCD_INTERFACE_H
 #define    CLCD_INTERFACE_H
 
+/*
+ * @fn		:	CLCD_Init
+ * @brief	:	Initializes the configurations the character 16x2 LCD
+ * @param	:	void
+ * @retval	:	void
+ *
+ * */
 void CLCD_Init(void);
+
+/*
+ * @fn		:	CLCD_SendCmd
+ * @brief	:	to send command to the character 16x2 LCD
+ * @param	:	Copy_u8Cmd 		(uint8_t)
+ * @retval	:	void
+ *
+ * */
 void CLCD_SendCmd(uint8_t Copy_u8Cmd);
+
+/*
+ * @fn		:	CLCD_SendData
+ * @brief	:	to send Data to the character 16x2 LCD
+ * @param	:	Copy_u8Data			(uint8_t)
+ * @retval	:	void
+ *
+ * */
 void CLCD_SendData(uint8_t Copy_u8Data);
-void CLCD_PrintWord(char* Copy_pvData);
+
+/*
+ * @fn		:	CLCD_PrintChar
+ * @brief	:	to send data (of type char) to the character 16x2 LCD
+ *  										you can send anything from the asci table
+ * @param	:	Copy_u8Data  -> for example : 'a'  , 'A' ,or '0'
+ * @retval	:	void
+ *
+ * */
+void CLCD_PrintChar(uint8_t Copy_u8Data);
+
+/*
+ * @fn		:	CLCD_PrintString
+ * @brief	:	to send data (of type String) to the character 16x2 LCD
+ *  						you can send anything between " "
+ * @param	:	Copy_pvData  pointer to string -> for example : "Driver"  ,or "Data is 6/3/2024"
+ * @retval	:	void
+ *
+ * */
+void CLCD_PrintString(char* Copy_pvData);
+
+/*
+ * @fn		:	CLCD_PrintNumber
+ * @brief	:	to send data numbers (of type int32_t) to the character 16x2 LCD
+ *  						you can send anything between " "
+ * @param	:	Copy_u32Number  	pointer to string -> for example : "1234"
+ * @retval	:	void
+ *
+ * */
+void CLCD_PrintNumber(int32_t Copy_u32Number);
+
+/*
+ * @fn		:	CLCD_GoToXY
+ * @brief	:	to set the courser location
+ * @param	:	Copy_u8XAddress  	(uint8_t)
+ * @param	:	Copy_u8YAddress  	(uint8_t)
+ * @retval	:	void
+ *
+ * */
+void CLCD_GoToXY(uint8_t Copy_u8XAddress ,uint8_t Copy_u8YAddress );
+
+/*
+ * @fn		:	CLCD_ClearDisplay
+ * @brief	:	to clear the display of the character 16*2 LCD
+ * @param	:	void
+ * @retval	:	void
+ *
+ * */
+void CLCD_ClearDisplay(void);
 
 #endif
--- 03-HAL/03-CLCD/Src/CLCD_prog.c
+++ 03-HAL/03-CLCD/Src/CLCD_prog.c
@@ -1,11 +1,11 @@
 /********************************************************************************
  ********************************************************************************
  ********************************************************************************
- * @File	:	I2C_prog.c
+ * @File	:	CLCD_prog.c
  * @Author	:	Usef Elbedwehy
- * @Brief	:	Main program body of I2C
- * @layer	:	MCAL
- * @Date	:	1 March 2024
+ * @Brief	:	Main program body of CLCD
+ * @layer	:	HAL
+ * @Date	:	6 March 2024
  ********************************************************************************
  ********************************************************************************
  ********************************************************************************
@@ -27,6 +27,14 @@
 
 
 /********************************************************************************/
+
+/*
+ * @fn		:	CLCD_Init
+ * @brief	:	Initializes the configurations the character 16x2 LCD
+ * @param	:	void
+ * @retval	:	void
+ *
+ * */
 void CLCD_Init(void)
 {
 	PinConfig_t CLCD_PinD4 = {
@@ -82,14 +90,22 @@ void CLCD_Init(void)
 	/*FUNCTION SET*/
 	CLCD_SendCmd(FUNCTION_SET);
 	/*DISPLAY ON/OFF CONTROL*/
-		CLCD_SendCmd(DISPLAY_ON_OFF_CONTROL);
+	CLCD_SendCmd(DISPLAY_ON_OFF_CONTROL);
 	/*DISPLAY CLEAR*/
 	CLCD_SendCmd(DISPLAY_CLEAR);
 	/*ENTRY MODE SET*/
 	CLCD_SendCmd(RETURN_HOME);
-	SYSTICK_DelayMs(50);
+	SYSTICK_DelayMs(2);
 }
 
+
+/*
+ * @fn		:	CLCD_SendCmd
+ * @brief	:	to send command to the character 16x2 LCD
+ * @param	:	Copy_u8Cmd 		(uint8_t)
+ * @retval	:	void
+ *
+ * */
 void CLCD_SendCmd(uint8_t Copy_u8Cmd)
 {
 	/*D4~D7 are connected as unusual: D7->B12 .... D4->B15
@@ -124,6 +140,14 @@ void CLCD_SendCmd(uint8_t Copy_u8Cmd)
 
 
 }
+
+/*
+ * @fn		:	CLCD_SendData
+ * @brief	:	to send Data to the character 16x2 LCD
+ * @param	:	Copy_u8Data			(uint8_t)
+ * @retval	:	void
+ *
+ * */
 void CLCD_SendData(uint8_t Copy_u8Data)
 {
 	/*D4~D7 are connected as unusual: D7->B12 .... D4->B15
@@ -157,14 +181,107 @@ void CLCD_SendData(uint8_t Copy_u8Data)
 	GPIO_u8SetPinValue(CLCD_CONTROL_PORT, CLCD_EN, PIN_LOW);
 }
 
+/*
+ * @fn		:	CLCD_PrintChar
+ * @brief	:	to send data (of type char) to the character 16x2 LCD
+ *  										you can send anything from the asci table
+ * @param	:	Copy_u8Data  -> for example : 'a'  , 'A' ,or '0'
+ * @retval	:	void
+ *
+ * */
+void CLCD_PrintChar(uint8_t Copy_u8Data)
+{
+	CLCD_SendData(Copy_u8Data);
+}
 
-void CLCD_PrintWord(char* Copy_pvData)
+
+/*
+ * @fn		:	CLCD_PrintString
+ * @brief	:	to send data (of type String) to the character 16x2 LCD
+ *  						you can send anything between " "
+ * @param	:	Copy_pvData  pointer to string -> for example : "Driver"  ,or "Data is 6/3/2024"
+ * @retval	:	void
+ *
+ * */
+void CLCD_PrintString(char* Copy_pvData)
 {
 	uint8_t Local_u8Counter = 0;
 	while(Copy_pvData[Local_u8Counter] != '\0')
 	{
 		CLCD_SendData(Copy_pvData[Local_u8Counter]);
 		Local_u8Counter++;
-		SYSTICK_DelayMs(50);
+
 	}
+}
+
+/*
+ * @fn		:	CLCD_PrintNumber
+ * @brief	:	to send data numbers (of type int32_t) to the character 16x2 LCD
+ *  						you can send anything between " "
+ * @param	:	Copy_u32Number  	pointer to string -> for example : "1234"
+ * @retval	:	void
+ *
+ * */
+void CLCD_PrintNumber(int32_t Copy_u32Number)
+{
+
+	uint8_t Local_u8i =0;
+	uint8_t Local_u8Counter =0;
+	uint8_t Local_u8ReverseNum[8];
+	if(Copy_u32Number == 0)
+	{
+		CLCD_SendData('0');
+	}
+	else if(Copy_u32Number < 0)
+	{
+		CLCD_SendData('-');
+		Copy_u32Number *= (-1) ;
+	}
+
+	while(Copy_u32Number != 0)
+	{
+		Local_u8ReverseNum[Local_u8Counter] = (Copy_u32Number % 10) ;
+		Copy_u32Number = Copy_u32Number / 10;
+		Local_u8Counter ++;
+	}
+	for(Local_u8i=(Local_u8Counter); Local_u8i > 0 ; Local_u8i--)
+	{
+		CLCD_SendData( Local_u8ReverseNum[Local_u8i-1] + '0');
+	}
+
+}
+
+/*
+ * @fn		:	CLCD_GoToXY
+ * @brief	:	to set the courser location
+ * @param	:	Copy_u8XAddress  	(uint8_t)
+ * @param	:	Copy_u8YAddress  	(uint8_t)
+ * @retval	:	void
+ *
+ * */
+void CLCD_GoToXY(uint8_t Copy_u8XAddress ,uint8_t Copy_u8YAddress )
+{
+	uint8_t Local_CurrentAddress = 0;
+	switch(Copy_u8XAddress)
+	{
+	case 0:
+		Local_CurrentAddress = Copy_u8YAddress;
+		break;
+	case 1:
+		Local_CurrentAddress = Copy_u8YAddress + 0x40;
+		break;
+	}
+	CLCD_SendCmd(Local_CurrentAddress + 128);
+}
+
+/*
+ * @fn		:	CLCD_ClearDisplay
+ * @brief	:	to clear the display of the character 16*2 LCD
+ * @param	:	void
+ * @retval	:	void
+ *
+ * */
+void CLCD_ClearDisplay(void)
+{
+	CLCD_SendCmd(DISPLAY_CLEAR);
 }
